137d22ae294cc048b06c51a1f292eae8fd0732ca
akase <creme0123@gmail.com>
2021-10-09 15:15:53 +0900

    コメントを追加

--- www/html/admin_change_stock.php
+++ www/html/admin_change_stock.php
@@ -1,25 +1,39 @@
 <?php
+//定数ファイル読み込み
 require_once '../conf/const.php';
+//汎用関数ファイル読み込み
 require_once MODEL_PATH . 'functions.php';
+//userデータに関する関数ファイル読み込み
 require_once MODEL_PATH . 'user.php';
+//itemデータに関する関数ファイル読み込み
 require_once MODEL_PATH . 'item.php';
 
+//ログインチェックを行うためにセッション開始
 session_start();
 
+//ログインチェック関数を利用
 if(is_logined() === false){
+  //ログインされていない場合はログインページにリダイレクト
   redirect_to(LOGIN_URL);
 }
 
+//PDOを取得
 $db = get_db_connect();
 
+//PDOを利用してログインユーザーのデータを取得
 $user = get_login_user($db);
 
+//アドミンチェック関数を利用
 if(is_admin($user) === false){
+  //アドミンではない場合はログインページにリダイレクト
   redirect_to(LOGIN_URL);
 }
 
+//item_idを取得
 $item_id = get_post('item_id');
+//stockを取得
 $stock = get_post('stock');
+
 
 if(update_item_stock($db, $item_id, $stock)){
   set_message('在庫数を変更しました。');
